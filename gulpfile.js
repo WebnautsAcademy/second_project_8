243cc68bf0529eebc2999e29e5fcfe8da0700036
Roman Holovchenko <GolovchenkoR777@gmail.com>
2021-07-16 23:34:52 +0300

    разобрался со сборщиком)

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,48 @@
 //Подключаем галп
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const plumber = require("gulp-plumber");
+const less = require("gulp-less");
+const browserSync = require("browser-sync").create();
 
-gulp.task('html', () => {
-  return gulp.src('source/*.html')
-    .pipe(gulp.dest('./build'));
+gulp.task("html", () => {
+  return gulp.src("source/*.html").pipe(gulp.dest("./build"));
 });
 
+gulp.task("css", function () {
+  return (
+    gulp
+      .src("source/less/style.less")
+      // обработчик ошибок
+      .pipe(plumber())
+      .pipe(less())
+      .pipe(gulp.dest("build/css"))
+  );
+});
 
-gulp.task('watch', () => {
+gulp.task("imagemin", function () {
+  return gulp
+    .src("source/img/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./build/img"));
+});
+
+gulp.task("watch", () => {
   browserSync.init({
     server: {
-      baseDir: "./build"
-    }
+      baseDir: "./build",
+    },
   });
 
-  gulp.watch('./source/*.html', gulp.series('html'))
-  gulp.watch("./source/*.html").on('change', browserSync.reload);
+  gulp.watch("./source/less/**/*.less", gulp.series("css"));
+  gulp.watch("./source/img/**", gulp.series("imagemin"));
+  gulp.watch("./source/*.html", gulp.series("html"));
+  gulp.watch("./source/less/**/*.less").on("change", browserSync.reload);
+  gulp.watch("./source/*.html").on("change", browserSync.reload);
 });
 
 //Таск по умолчанию, Запускает del, styles, scripts и watch
-gulp.task('start', gulp.series('watch', 'html'));
+gulp.task(
+  "start",
+  gulp.series(gulp.parallel("imagemin", "html", "css"), "watch")
+);
